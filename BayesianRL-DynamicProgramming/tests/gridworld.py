2ed0749f2d99262dd390431ae02f2b21331aced0
Abhijeet Sahu <abhijeetapplication@gmail.com>
2022-03-14 22:10:20 -0500

    Delete gridworld.py

--- BayesianRL-DynamicProgramming/tests/gridworld.py
+++
@@ -1,8 +0,0 @@
-"""
-Prints an example of a grid world. See gridworld.py for a key to the symbols.
-"""
-from bayesrl.environments import GridWorld
-
-maze = GridWorld.samples['larger']
-for row in maze:
-    print(row)
